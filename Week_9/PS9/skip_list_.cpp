c93c3506759f638ba83778f6f57f65d3f2518227
Lev0071 <coder0071@outlook.com>
2025-07-01 13:12:13 +1000

    PS9 resources

---
+++ Week_9/PS9/skip_list_.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <cstring> // memset
+#include <cstdlib> // rand
+#include <climits>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+template<typename T,typename U>
+class Node
+{
+private:
+    T key; // key
+    U value; // Value
+
+public:
+    vector<shared_ptr<Node<T, U>>> forward;
+
+    Node(T k,U val, int level) : key(k), value(val),forward(level + 1, nullptr) {}
+
+    T getKey() const { return key; }
+    U getValue() const { return value; }
+
+    ~Node()
+    {
+        cout << "Node(key: " << key << " ,value: " << value << " destroyed.\n";
+    }
+};
+
+template<typename T,typename U>
+class SkipList
+{
+private:
+    int MAXLVL; // Maximum level of the skip list
+    float P;    // Probability of node promotion to higher levels (typically 0.5).
+    int level;
+    shared_ptr<Node<T, U>> header;
+
+public:
+    SkipList(int, float);
+    bool searchElement(T);
+    void insertElement(T);
+    bool deleteElement(T);
+    void displayList();
+    int randomLevel();
+    ~SkipList()
+    {
+        cout << "skip list destroyed.\n";
+    }
+};
+
+template<typename T, typename U>
+SkipList<T, U>::SkipList(int MAXLVL, float P) : MAXLVL(MAXLVL), P(P), level(0)
+{
+    header = make_shared<Node<T, U>>(INT_MIN,U{}, MAXLVL);
+}
+
+template<typename T, typename U>
+int SkipList<T, U>::randomLevel()
+{
+    // rand() generates an integer in the range [0, RAND_MAX]
+    float r = (float)rand() / RAND_MAX;
+    int lvl = 0;
+    while (r < P && lvl < MAXLVL)
+    {
+        lvl++;
+        r = (float)rand() / RAND_MAX;
+    }
+    return lvl;
+}
+
+template<typename T, typename U>
+bool SkipList<T, U>::searchElement(T key)
+{
+    auto current = header;
+    cout << "Search Path:" << endl;
+    for (int i = level; i >= 0; i--)
+    {
+        while (current->forward[i] && current->forward[i]->getKey() < key)
+        {
+            current = current->forward[i];
+        }
+    }
+    current = current->forward[0];
+
+    return current && current->getKey() == key;
+}
+
+template<typename T, typename U>
+void SkipList<T, U>::insertElement(T key, U value)
+{
+    auto current = header;
+    vector<shared_ptr<Node<T, U>>> update(MAXLVL + 1, nullptr);
+    for (int i = level; i >= 0; i--)
+    {
+        while (current->forward[i] && current->forward[i]->getKey() < key)
+        {
+            current = current->forward[i];
+        }
+        update[i] = current;
+    }
+    current = current->forward[0];
+    if (!current || current->getKey() != key)
+    {
+        int rlevel = randomLevel();
+        if (rlevel > level)
+        {
+            for (int i = level + 1; i <= rlevel; i++)
+            {
+                update[i] = header;
+            }
+            level = rlevel;
+        }
+        auto n = make_shared<Node<T,U>>(key, value, rlevel);
+        for (int i = 0; i <= rlevel; i++)
+        {
+            n->forward[i] = update[i]->forward[i];
+            update[i]->forward[i] = n;
+        }
+        cout << "Successfully Inserted key " << key << "\n";
+    }
+}
+
+template<typename T, typename U>
+bool SkipList<T, U>::deleteElement(T key)
+{
+    auto current = header;
+    vector<shared_ptr<Node<T, U>>> update(MAXLVL + 1, nullptr);
+    for (int i = level; i >= 0; i--)
+    {
+        while (current->forward[i] && current->forward[i]->getKey() < key)
+        {
+            current = current->forward[i];
+        }
+        update[i] = current;
+    }
+    current = current->forward[0];
+
+    if (current && current->getKey() == key)
+    {
+        for (int i = 0; i <= level; i++)
+        {
+            if (update[i]->forward[i] != current)
+                break;
+            update[i]->forward[i] = current->forward[i];
+        }
+        while (level > 0 && !header->forward[level])
+        {
+            level--;
+        }
+        cout << "Successfully deleted key " << key << "\n";
+        return true;
+    }
+    return false;
+}
+
+
+
+
+int main()
+{
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    LogSystem logSys;
+    logSys.insert(1620000001, "System started");
+    logSys.insert(1620000012, "User logged in");
+    logSys.insert(1620000020, "Error detected");
+
+    cout << "Search 1620000012: " << logSys.search(1620000012) << "\n";
+    cout << "Search 1620000999: " << logSys.search(1620000999) << "\n";
+
+    logSys.display();
+
+    cout << "Remove 1620000020: " << (logSys.remove(1620000020) ? "Deleted" : "Not Found") << "\n";
+    logSys.display();
+
+    return 0;
+}
+
+// g++ -std=c++17 PS8.cpp -o PS8
