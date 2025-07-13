c13e63c915d3868c8f819f658411e43e74dbb8d3
Lev0071 <coder0071@outlook.com>
2025-07-13 19:13:54 +1000

    add res

---
+++ Milestone/Queries/Interface refernce.cpp
@@ -0,0 +1,93 @@
+printf("******************************\n");
+printf("* Menu - Level 1             *\n");
+printf("* Select an option:          *\n");
+printf("* 1. Encrypt a message       *\n");
+printf("* 2. Decrypt a message       *\n");
+printf("* 3. Quit                    *\n");
+printf("******************************\n");
+
+printf("****************************************\n");
+printf("* Menu - Level 2: Encryption           *\n");
+printf("* Select an option:                    *\n");
+printf("* 1. Enter a message                   *\n");
+printf("* 2. One-round encryption              *\n");
+printf("* 3. Automatic multi-round encryption  *\n");
+printf("* 4. Back                               *\n");
+printf("****************************************\n");
+
+printf("************************************\n");
+printf("* Menu - Level 2: Decryption       *\n");
+printf("* Select an option:                *\n");
+printf("* 1. Enter a message               *\n");
+printf("* 2. Enter the round number        *\n");
+printf("* 3. For each round, print the grid*\n");
+printf("*    and the corresponding decoded *\n");
+printf("*    message                       *\n");
+printf("* 4. Back                          *\n");
+printf("************************************\n");
+
+printf("***********************************************\n");
+printf("* Menu - Level 3: Encryption                  *\n");
+printf("* Select an option:                           *\n");
+printf("* 1. Enter a grid size                        *\n");
+printf("* 2. Automatic grid size                      *\n");
+printf("* 3. Print the grid and the encoded message   *\n");
+printf("* 4. Back                                     *\n");
+printf("***********************************************\n");
+
+printf("***********************************************\n");
+printf("* Menu - Level 3: Encryption                  *\n");
+printf("* Select an option:                           *\n");
+printf("* 1. Enter the round number                   *\n");
+printf("* 2. For each round, print the grid and       *\n");
+printf("*    the corresponding encoded message        *\n");
+printf("* 3. Back                                     *\n");
+printf("***********************************************\n");
+
+// ------------------------
+
+/*L1_1*/printf("******************************\n");
+/*L1_2*/printf("* Menu - Level 1             *\n");
+/*L1_3*/printf("* Select an option:          *\n");
+/*L1_4*/printf("* 1. Encrypt a message       *\n");
+/*L1_5*/printf("* 2. Decrypt a message       *\n");
+/*L1_6*/printf("* 3. Quit                    *\n");
+/*L1_7*/printf("******************************\n");
+
+/*L2_E1*/printf("****************************************\n");
+/*L2_E2*/printf("* Menu - Level 2: Encryption           *\n");
+/*L2_E3*/printf("* Select an option:                    *\n");
+/*L2_E4*/printf("* 1. Enter a message                   *\n");
+/*L2_E5*/printf("* 2. One-round encryption              *\n");
+/*L2_E6*/printf("* 3. Automatic multi-round encryption  *\n");
+/*L2_E7*/printf("* 4. Back                               *\n");
+/*L2_E8*/printf("****************************************\n");
+
+/*L2_D1*/printf("************************************\n");
+/*L2_D2*/printf("* Menu - Level 2: Decryption       *\n");
+/*L2_D3*/printf("* Select an option:                *\n");
+/*L2_D4*/printf("* 1. Enter a message               *\n");
+/*L2_D5*/printf("* 2. Enter the round number        *\n");
+/*L2_D6*/printf("* 3. For each round, print the grid*\n");
+/*L2_D7*/printf("*    and the corresponding decoded *\n");
+/*L2_D8*/printf("*    message                       *\n");
+/*L2_D9*/printf("* 4. Back                          *\n");
+/*L2_D10*/printf("************************************\n");
+
+/*L3_E1*/printf("***********************************************\n");
+/*L3_E2*/printf("* Menu - Level 3: Encryption                  *\n");
+/*L3_E3*/printf("* Select an option:                           *\n");
+/*L3_E4*/printf("* 1. Enter a grid size                        *\n");
+/*L3_E5*/printf("* 2. Automatic grid size                      *\n");
+/*L3_E6*/printf("* 3. Print the grid and the encoded message   *\n");
+/*L3_E7*/printf("* 4. Back                                     *\n");
+/*L3_E8*/printf("***********************************************\n");
+
+/*L3_ME1*/printf("***********************************************\n");
+/*L3_ME2*/printf("* Menu - Level 3: Encryption                  *\n");
+/*L3_ME3*/printf("* Select an option:                           *\n");
+/*L3_ME4*/printf("* 1. Enter the round number                   *\n");
+/*L3_ME5*/printf("* 2. For each round, print the grid and       *\n");
+/*L3_ME6*/printf("*    the corresponding encoded message        *\n");
+/*L3_ME7*/printf("* 3. Back                                     *\n");
+/*L3_ME8*/printf("***********************************************\n");
